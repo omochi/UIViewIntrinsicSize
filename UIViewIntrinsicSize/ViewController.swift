c0aefec211d92c688981aedd2800ad824c54402c
omochimetaru <omochi.metaru@gmail.com>
2018-12-06 17:38:45 +0900

    init

---
+++ UIViewIntrinsicSize/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewIntrinsicSize
+//
+//  Created by omochimetaru on 2018/12/06.
+//  Copyright © 2018 omochimetaru.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
